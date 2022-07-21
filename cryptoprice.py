081cef91294dc0073bfe46c141f60ca5238daa91
kiishyen <49295431+kiishyen@users.noreply.github.com>
2022-07-21 18:23:39 +0800

    Update cryptoprice.py

--- cryptoprice.py
+++ cryptoprice.py
@@ -10,8 +10,11 @@ This app shows you Top 5 Cryptocurrency Price and it's Market Supply
 st.image('https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/cryptocurrency-trends_-is-bitcoin-mining-profitable-in-2021162075307076393.jpg')
 
 
+option = st.sidebar.selectbox(
+    'Select a Cryptocurrency',
+     ['Bitcoin', 'Ethereum', 'Solana', 'BNB', 'Wrapped Bitcoin'])
+if option=='Bitcoin':
+  st.header('Bitcoin')
+  st.write('Price(USD) = 1,866,276')
+  st.write('Market Cap = 35,577,749,997,460.60')
 
-# read data into a DataFrame
-import pandas as pd
-data = pd.read_csv('API.csv', sep=';')
-data.head()
