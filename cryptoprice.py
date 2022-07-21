4ee235e0e1ae85144e24ce13fd6d4a272256f2f4
kiishyen <49295431+kiishyen@users.noreply.github.com>
2022-07-21 17:51:27 +0800

    Update cryptoprice.py

--- cryptoprice.py
+++ cryptoprice.py
@@ -4,7 +4,18 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Cryptocurrency Price Monitor
-This app shows you Cryptocurrency Price and it's Market Supply
+# Top 5 Cryptocurrency Price Monitor
+This app shows you Top 5 Cryptocurrency Price and it's Market Supply
 """)
 st.image('https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/cryptocurrency-trends_-is-bitcoin-mining-profitable-in-2021162075307076393.jpg')
+
+
+option = st.sidebar.selectbox(
+    'Select Cryptocurrency',
+     ['Bitcoin', 'Ethereum', 'BNB', 'Solana', 'Wrapped Bitcoin'])
+if option=='Bitcoin':
+  df = pd.DataFrame('Price in USD'(1,866,276),0)
+                    
+
+# Display a static table
+st.dataframe(df)
