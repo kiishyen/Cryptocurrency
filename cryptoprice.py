abae8004edea8985028b57d9da240fc4966769f1
kiishyen <49295431+kiishyen@users.noreply.github.com>
2022-07-21 17:26:44 +0800

    Create cryptoprice.py

---
+++ cryptoprice.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Cryptocurrency Price Monitor
+This app shows you Cryptocurrency Price and it's Market Supply** type!
+""")
+st.image('https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/cryptocurrency-trends_-is-bitcoin-mining-profitable-in-2021162075307076393.jpg')
