cc6e40f1a18e4ca5c650f7f50ae151233619fed9
kiishyen <49295431+kiishyen@users.noreply.github.com>
2022-07-21 18:37:49 +0800

    Update cryptoprice.py

--- cryptoprice.py
+++ cryptoprice.py
@@ -14,7 +14,30 @@ option = st.sidebar.selectbox(
     'Select a Cryptocurrency',
      ['Bitcoin', 'Ethereum', 'Solana', 'BNB', 'Wrapped Bitcoin'])
 if option=='Bitcoin':
-  st.header('Bitcoin')
+  st.header('Bitcoin, BTC')
   st.write('Price(USD) = 1,866,276')
-  st.write('Market Cap = 35,577,749,997,460.60')
+  st.write('Market Cap(USD) = 35,577,749,997,460.60')
 
+elif option=='Ethereum':
+    st.header('Ethereum, ETH')
+  st.write('Price(USD) = 106,307')
+  st.write('Market Cap(USD) = 12,875,584,996,925.90')
+    
+    elif option=='Solana':
+        st.header('Solana, SOL')
+  st.write('Price(USD) = 2,396')
+  st.write('Market Cap(USD) = 13,537,594,562')
+    
+    elif option=='BNB':
+        st.header('Binance, BNB')
+  st.write('Price(USD) = 17,867')
+  st.write('Market Cap(USD) = 46,801,210,081')
+    
+    else option=='Wrapped Bitcoin':
+        st.header('Wrapped Bitcoin, WBTC')
+  st.write('Price(USD) = 1,864,354')
+  st.write('Market Cap(USD) = 8,241,528,653')
+        
+        
+        
+    
